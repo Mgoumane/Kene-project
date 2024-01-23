68aefc08b3d9f2ea47c3a64a11ec8c508dbc08c6
Mgoumane <goumane.mahmoudou@gmail.com>
2024-01-23 10:51:23 +0100

    version  0.0.3 first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Header';
 import ButtonPanier from './components/ButtonPanier';
 import ListParfums from './components/ListParfums';
 import Footer from './components/Footer';
-import Panier from './pages/Panier';
+import Panier  from './pages/Panier';
 
 import { useState } from 'react';
 
